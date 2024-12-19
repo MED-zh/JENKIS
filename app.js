f58e362db85374a4656308fe75ee5287b660a88f
MED-zh <zahouane000@gmail.com>
2024-12-19 18:50:17 +0100

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const server = http.createServer((req, res) => {
   res.end('Hello, world  i am mohamed zahouane \n');
 });
 
-const PORT = 3000;
+const PORT = 3001;
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
 })
